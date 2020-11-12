f5b95f39b100d2135f5457a3a7a83248c6066d08
Ivan-Danilov996 <ivandanilov996@yandex.ru>
2020-11-12 12:24:58 +0300

    initial commit

---
+++ e2e/e2e.test.js
@@ -0,0 +1,46 @@
+import puppetteer from 'puppeteer';
+import { fork } from 'child_process';
+
+jest.setTimeout(30000); // default puppeteer timeout
+
+describe('show pupover', () => {
+  let browser = null;
+  let page = null;
+  let server = null;
+  const baseUrl = 'http://localhost:9000';
+
+  beforeAll(async () => {
+    server = fork(`${__dirname}/e2e.server.js`);
+    await new Promise((resolve, reject) => {
+      server.on('error', reject);
+      server.on('message', (message) => {
+        if (message === 'ok') {
+          resolve();
+        }
+      });
+    });
+
+    browser = await puppetteer.launch({
+      // headless: false, // show gui
+      // slowMo: 250,
+      // devtools: true, // show devTools
+    });
+    page = await browser.newPage();
+  });
+
+  afterAll(async () => {
+    await browser.close();
+    server.kill();
+  });
+
+  test('should add do something', async () => {
+    await page.goto(baseUrl);
+  });
+
+  test('show pupover', async () => {
+    await page.goto(baseUrl);
+    const button = await page.$('.button');
+    button.click();~
+    await page.waitForSelector('.pupover.show');
+  });
+});
